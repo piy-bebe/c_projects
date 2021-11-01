7c27353d5507f1bc16aff9f46b238498647e1520
piy-bebe <piy.bebe@gmail.com>
2021-11-01 14:02:29 +0300

    modified src

--- project2/Source.cpp
+++ project2/Source.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	setlocale(0, "");
 
-	cout << "Hello World 123 :)";
+	cout << "Привет мир :)";
 	cout << "Hello World 123 :)";
 	cout << "Hello World 123 :)";
 	cout << "Hello World 123 :)";
