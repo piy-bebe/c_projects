c7f8447a0cbe07d27ff1008f15f52fa4d15934bd
piy-bebe <piy.bebe@gmail.com>
2021-11-02 14:16:56 +0300

    created project4

---
+++ project4/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	cout << "Hello World";
+	cout << "Hello World";
+	cout << "Hello World";
+	cout << "Hello World";
+	cout << "Hello World";
+
+	return 0;
+}
