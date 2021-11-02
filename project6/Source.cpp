101550a87e9d18664723bae570e0c50dd29da0af
piy-bebe <piy.bebe@gmail.com>
2021-11-02 14:39:26 +0300

    created project6

---
+++ project6/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int func()
+{
+	int a = 20;
+	int b = 20;
+	int result = (a + b) * 2;
+
+	if (result > 59)
+	{
+		return 90;
+	}
+	else
+	{
+		return -200;
+	}
+}
+
+int sum()
+{
+
+	return 20;
+}
+
+int main()
+{
+	setlocale(0, "");
+
+	system("color 0c");
+
+	return 0;
+}
